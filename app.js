3d1978dd51779999aad13a93b8a126f635da4a36
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2025-11-26 01:25:17 +0530

    update to redirect link

--- app.js
+++ app.js
@@ -8,33 +8,37 @@ const notFound = require("./middleware/not-found.js")
 const errorHandler = require("./middleware/error-handler.js")
 const helmet = require("helmet")
 const YAML = require("yamljs")
-const swaggerUi=require('swagger-ui-express');
-
+const swaggerUi = require('swagger-ui-express')
 
 // health for render
-app.get("/health" , (req,res) =>{
+app.get("/health", (req, res) => {
   res.status(200).send("OK")
 })
 
 // middleware
-
 app.use(helmet())
-app.use(express.static('./public'));
+app.use(express.static('./public'))
 app.use(express.json())
 
-const swaggerFilePath = path.resolve(__dirname,'swagger.yaml');
-// Load your Swagger YAML file
+const swaggerFilePath = path.resolve(__dirname, 'swagger.yaml');
 const swaggerDocument = YAML.load(swaggerFilePath);
-app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', swaggerUi.setup(swaggerDocument));
- 
-app.use("/api/v1/tasks" , tasks)
+
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// ⭐ ⭐ REDIRECT ROOT TO SWAGGER ⭐ ⭐
+app.get('/', (req, res) => {
+  res.redirect('/api-docs')
+})
+
+app.use("/api/v1/tasks", tasks)
+
+// Not found + error handler
 app.use(notFound)
 app.use(errorHandler)
 
 const port = process.env.PORT || 3000
 
-connectDB().then(()=>{
-    app.listen(port, () => console.log(`Server running on ${port}`));
-});
-
+connectDB().then(() => {
+  app.listen(port, () => console.log(`Server running on ${port}`))
+})
